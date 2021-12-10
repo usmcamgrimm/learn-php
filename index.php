6fd9d94ee612331840b1e4e85743c86d6492153a
Adrian Grimm <usmcamgrimm@gmail.com>
2021-12-10 12:17:38 -0500

    L7 - Arrays

--- index.php
+++ index.php
@@ -1,27 +1,56 @@
 <?php
 
-$radius = 25; # integer
-$pi = 3.14; # float
+// indexed arrays
+$peopleOne = ['adrian', 'jessica', 'colton'];
+// echo $peopleOne[1]; # jessica
 
-// Basic Operators + - * / **
+$peopleTwo = array('anakin', 'obi-wan');
+// echo $peopleTwo[1]; # obi-wan
 
-// echo $pi * $radius ** 2
-// echo 2 * (4 + 9) / 3; # returns 8.6666666666667
+$ages = [1, 12, 19, 24];
+# print_r => 'print readable'
+// print_r($ages); # Array ( [0] => 1 [1] => 12 [2] => 19 [3] => 24 )
 
-// increment and decrement operators
-// echo $radius++;
-// echo $radius--;
+$ages[1] = 13; # changing the value of an entry in an array
+// print_r($ages); # Array ( [0] => 1 [1] => 13 [2] => 19 [3] => 24 )
 
-// Shorthand operators
-$age = 20;
-// echo $age += 10; # equivalent to $age = $age + 10;
-// echo $age -= 10; # equivalent to $age = $age - 10;
-// echo $age *= 2; #equivalent to $age = $age * 2;
+# adding a new value to the array
+$ages[] = 45;
+// print_r($ages); # Array ( [0] => 1 [1] => 13 [2] => 19 [3] => 24 [4] => 45 )
+array_push($ages, 46);
+// print_r($ages); # Array ( [0] => 1 [1] => 13 [2] => 19 [3] => 24 [4] => 45 [5] => 46 )
 
-// functions with numbers
-// echo floor($pi); # round to nearest integer -> returns 3
-// echo ceil($pi); # round up to the nearest integer -> returns 4
-echo pi(); # returns the full value of pi
+# Determine the length of an array
+// echo count($ages); #returns 6
+
+# Merge arrays together
+$peopleThree = array_merge($peopleOne, $peopleTwo);
+// print_r($peopleThree); # Array ( [0] => adrian [1] => jessica [2] => colton [3] => anakin [4] => obi-wan )
+
+// associative arrays
+# Key-Value Pairs => Use Keyts instead of indexes
+$lightsaberOne = ['anakin' => 'blue', 'luke' => 'green', 'mace' => 'purple'];
+// echo $jediOne['luke']; # returns green
+// print_r($jediOne); # Array ( [anakin] => blue [luke] => green [mace] => purple )
+
+$lightsaberTwo = array('vader' => 'red', 'rey' => 'yellow', 'yoda' => 'blue');
+// print_r($lightsaberTwo); # Array ( [vader] => red [rey] => yellow [yoda] => blue )
+
+$lightsaberTwo['obi-wan'] = 'blue'; # Adding obi-wan => blue
+// print_r($lightsaberTwo); # Array ( [vader] => red [rey] => yellow [obi-wan] => blue )
+
+# Override a value
+$lightsaberTwo['yoda'] = 'green';
+// print_r($lightsaberTwo); # Array ( [vader] => red [rey] => yellow [yoda] => green [obi-wan] => blue )
+
+# Determine the length of an array
+// echo count($lightsaberOne); # returns 3
+
+# Merge arrays together
+$lightsaberThree = array_merge($lightsaberOne, $lightsaberTwo);
+print_r($lightsaberThree); # Array ( [anakin] => blue [luke] => green [mace] => purple [vader] => red [rey] => yellow [yoda] => green [obi-wan] => blue )
+
+
 
 ?>
 

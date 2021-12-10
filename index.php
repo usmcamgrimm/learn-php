6994603bc38ef2330d5956b56381c00ef8efc16c
Adrian Grimm <usmcamgrimm@gmail.com>
2021-12-10 10:17:38 -0500

    L4 - Strings & Variables

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
 
-  // echo 'hello, ninjas';
+define('NAME', 'Adrian'); # defines a constant
+// $name = "Adrian"; #defines a variable that you can overwrite
+$age = 45;
+
+// $name = 'Mario';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My First PHP File</title>
+  <title>PHP Tutorials</title>
 </head>
+
 <body>
-  <h1><?php echo 'Hello, ninjas'; ?></h1>
+  <h1>User Profile Page</h1>
+
+  <div><?php echo NAME; ?></div>
+  <div><?php echo $age; ?></div>
 </body>
+
 </html>
